370950291e556d04bee44c028992ed5c657d7348
Dinhmobile35 <136127701+Dinhmobile35@users.noreply.github.com>
2023-11-12 17:19:42 +0700

    Add files via upload

---
+++ dangnhap.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="profile.js"></script>
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="fontawesome-free-6.4.2-web/css/all.css">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <?php
+    include("ketnoi.php");
+    $error_message = "";
+    if (isset($_POST["login_btn"])) {
+        $EMAIL = $_POST["EMAIL"];
+        $MAT_KHAU = $_POST["MAT_KHAU"];
+
+        $sql_check = "SELECT EMAIL, MAT_KHAU FROM dangki WHERE EMAIL ='$EMAIL' AND MAT_KHAU='$MAT_KHAU'";
+        $res_check = $conn->query($sql_check);
+
+        if ($res_check->num_rows > 0) {
+            $chk = $_POST['chkRemeber'];
+            echo "rem=" . $chk;
+            if (isset($chk)) {
+                setcookie('taikhoan', $usr, time() + 60 * 60);
+            }
+
+            header("Location: webanime.php");
+        } else {
+            $error_message = "Sai mật khẩu hoặc tên đăng nhập không tồn tại vui lòng kiểm tra lại!!";
+        }
+    }
+    ?>
+    <?php if (!empty($error_message)) : ?>
+        <script>
+            alert("<?php echo $error_message ?>")
+            $(document).ready(function () {
+            $("#loginModal").show()});
+        </script>
+    <?php endif; ?>
+
+    <form action="" method="post">
+        <div id="loginModal" class="modal">
+            <div class="modal-content">
+                <span class="close" onclick="close_modal()">&times;</span>
+                <h2 class="titleModal">Đăng Nhập</h2>
+                <form class="login_content">
+                    <div class="input inputUsername">
+                        <label for="username">Email:</label>
+                        <input type="text" id="username" name="EMAIL" required><br>
+                    </div>
+                    <div class="input inputPassword">
+                        <label for="password">Mật khẩu:</label>
+                        <input type="password" id="password" name="MAT_KHAU" required><br>
+                    </div>
+                        <div>
+                            <span class="registerUser" >Chưa có tài khoản <button onclick="sigup()">Đăng kí ngay</button> </span>
+                        </div>
+                    <button type="submit" class="login_comfirm" name="login_btn" onclick="confirm_login()">Đăng Nhập</button>
+                </form>
+            </div>
+        </div>
+    </form>
+</body>
+
+</html>
---
+++ webanimeVN.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="profile.js"></script>
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="fontawesome-free-6.4.2-web/css/all.css">
+    <title>Shop anime VN</title>
+</head>
+<body>
+<?php
+    //include("ketnoi.php");
+   // require("dangki.php");
+    //require("dangnhap.php");
+   // require("header.php")
+    ?>
+    
+ <div id="logo1">
+        <video autoplay src="picture/logo/Kuru Kuru Kururin - Raphiiel.mp4" width="100px" height="100px"></video>
+                <a href="https://animevietsub.moe/"></br>SHOP ANIME VN</a>
+          </div> 
+     <div id="menu">
+           
+        <ul id="mainmenu">
+            <li>
+                <a href="https://pops.vn/anime">ANIME VÀ VIDEO</a>
+            </li>
+            <li>
+                <a href="">MANGA VÀ SÁCH</a>
+            </li>
+            <li>
+               <a href="">HÀNG HÓA</a>
+            </li>
+            <li>
+               <a href="">SỐ LIỆU VÀ ĐỒ SƯU TẦM </a>
+            </li>
+            <li>
+                <a href="">TRANG PHỤC</a>   
+            </li> 
+            <div id="taikhoan" >
+            <ul>
+            <li>
+                <a href="dangki.php">đăng kí </a>   
+            </li>  
+            <li>
+                <a href="dangnhap.php">đăng nhập</a>   
+            </li>
+            </ul>
+            </div>
+            <div id="timkiem">
+    <input type="text" name="q" placeholder="Tìm kiếm sản phẩm">
+    <button type="submit">Tìm kiếm</button>
+  </div>
+    </div>
+     
+     <div class="td1"> <p>Danh mục sản phẩm</p></div>
+    <div class="td2">SẢN PHẨM MỚI</div>
+    <ul class="ds">
+        <li><a href=#>&#8226;Mô hình</a></li>
+        <li><a href=#>&#8226;Đồ cosplay</a></li>
+        <li><a href=#>&#8226;Áo phông in hình nhân vật</a></li>
+        <li><a href=#>&#8226;Lót chuột</a></li>
+        <li><a href=#>&#8226;Hình dán anime</a></li>
+        <li><a href=#>&#8226;Tranh treo tường</a></li>
+</ul>
+<div class="td3"> <p>Sản Phẩm phụ kiện</p> </div>
+<ul class="ds">
+    
+    <li><a href=#> Quần áo anime</li>
+    <li><a href=#> Phụ kiện trang sức anime</a></li>
+    <li><a href=#> PC custom anime</a></li>
+    <li><a href=#> Ốp anime dành cho điện thoại android</a></li>
+    <li><a href=#> Ốp anime dành cho điện thoại iphone</a></li>
+</ul>
+<div class="content">
+    <ul>
+    
+    
+        <li>
+            <img src="picture/mushoku tensei/ph-11134201-7r98y-ln4wgxpu1hdrba.jpg" alt=""><br>
+            <p class="tensp"> <br>Mô hình nữ Roxy Migurida anime Thất Nghiệp Chuyển Sinh<br>tỉ lệ 1/20</p>
+            <p class="gia">2.000.000 VND</p>
+           <div class="bysubmit"> <button type="submit">Mua ngay</button> </div>
+       </li>
+
+        <li>
+            <img src="picture/mushoku tensei/650d42562ca06bad69e00d458e991e87.jpg" alt=""><br>
+            <p class="tensp">Mô hình Eris Boreas Greyrat Mushoku Tensei <br> tỉ lệ 1/20</p>
+            <p class="gia">2.500.000 VND</p>
+            <div class="bysubmit"> <button type="submit">Mua ngay</button> </div>
+        </li>
+        <li>
+            <img src="picture/sg-11134201-22120-2jtrl32e3jlv31.jpg" alt=""><br>
+            <p class="tensp"> Mô hình Megumin trong anime Konosuba FIGMA 407 khớp<br> cử động được 15 cm</p>
+            <p class="gia">4,490,000 VNĐ</p>
+            <div class="bysubmit"> <button type="submit">Mua ngay</button> </div>
+        </li>
+        
+        <li>
+            <img src="picture/11f0e72758eb9d7191129731c76df8d6.jpg" alt=""> <br>
+            <p class="tensp">Mô Hình Nhân Vật Dark Dragoon Forte  Forte The Devoted figure<br></p>
+            <p class="gia">4,790,000 VNĐ</p>
+            <div class="bysubmit"> <button type="submit">Mua ngay</button> </div>
+            
+        </li>
+        <li>
+            <img src="picture/vn-11134207-7r98o-lkpbtgw7i1zkdd.jpg" alt=""> <br>
+            <p class="tensp"> Mô hình Hitori Gotou bocchi the rock figure 8 cm có hộp <br> MNGF3ZP</p>
+            <p class="gia">2,990,000 VNĐ</p>
+            <div class="bysubmit"> <button type="submit">Mua ngay</button> </div>
+        </li>
+    </ul>
+    </div>
+    <div>
+  <footer>
+    <a href="#"><img class="logo2" src=" alt=""></a>
+    <div class="aboutus">
+<h3>About Us</h3><p>THẾ GIỚI DI ĐỘNG <br>
+ <p> © 2018. Công ty cổ phần Thế Giới Di Động. GPDKKD: 0303217354 </p><p>do sở KH & ĐT TP.HCM cấp ngày 02/01/2007. <p>GPMXH: 238/GP-BTTTT </p>do Bộ Thông Tin và Truyền Thông cấp ngày 04/06/2020.</p>
+
+     </p> 
+      </div>
+      <div class="hotline">
+        <h3>Hotline</h3>
+        <p>Phone :(+84) 999-999-999</p>
+        <p>Email: Dinhmboile@gmail.com</p>
+        <p> Địa chỉ: đường thanh bình ,hà đông , hà nội </p>
+      </div>
+  </footer>
+</div>
+</body>
+</html>
